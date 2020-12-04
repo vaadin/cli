dce282b5562d3680b8bcc9eeb748b747b54ae2f3
Artur Signell <artur@vaadin.com>
2020-12-04 13:44:31 +0200

    Support custom presets when you know what you are doing

--- src/vaadin-init.js
+++ src/vaadin-init.js
@@ -21,10 +21,13 @@ program
     "--git",
     "Initialize a Git repository for the project and commit the initial files"
   )
+  .option("--preset <preset>", "Use the given preset")
   .arguments("<projectName>")
   .action(async function (projectName) {
     const git = !!program.git;
-    const preset = program.pre
+    const preset = program.preset
+      ? program.preset
+      : program.pre
       ? "prerelease"
       : program.latest
       ? "latest"
@@ -36,33 +39,49 @@ program
       return;
     }
 
-    console.log(`Creating application '${projectName}' (${version})`);
+    console.log(`Creating application '${projectName}' (${preset})`);
 
     const options = {
       encoding: null,
       gzip: true,
     };
-    await request.get(
-      `https://start.vaadin.com/dl?preset=${preset}`,
-      options,
-      async function (_error, response, body) {
-        if (response && response.statusCode == 200) {
-          fs.writeFileSync("temp.zip", body);
-          await decompress("temp.zip", projectName, { strip: 1 });
-          fs.unlinkSync("temp.zip");
-          console.log("Project '" + projectName + "' created");
-          if (git) {
-            console.log("Creating Git repository and initial commit");
-            process.chdir(projectName);
+    await request
+      .get(
+        `https://start.vaadin.com/dl?preset=${preset}&projectName=${projectName}`,
+        options,
+        async function (_error, response, body) {
+          if (response && response.statusCode == 200) {
+            fs.writeFileSync("temp.zip", body);
+            try {
+              await decompress("temp.zip", ".", {
+                // workaround for https://github.com/kevva/decompress/issues/46
+                filter: (file) => !file.path.endsWith("/"),
+              });
+            } catch (e) {
+              console.error(e);
+              return;
+            }
+            fs.unlinkSync("temp.zip");
+            console.log("Project '" + projectName + "' created");
+            if (git) {
+              console.log("Creating Git repository and initial commit");
+              process.chdir(projectName);
 
-            require("simple-git")()
-              .init()
-              .add("./*")
-              .commit("Generated project");
+              require("simple-git")()
+                .init()
+                .add("./*")
+                .commit("Generated project");
+            }
           }
         }
-      }
-    );
+      )
+      .catch((e) => {
+        if (e.statusCode == 404) {
+          console.error("Preset not found");
+        } else {
+          console.error("Unable to create project: " + e.statusCode);
+        }
+      });
   });
 
 program.parse(process.argv);
