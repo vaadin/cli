d9820bc9c90ea8147540b635cc0b6bda09d28301
Artur Signell <artur@vaadin.com>
2020-12-04 13:19:53 +0200

    Download from start.vaadin.com

--- src/vaadin-init.js
+++ src/vaadin-init.js
@@ -4,129 +4,48 @@
 
 const fs = require("fs");
 const request = require("request-promise");
-const opn = require("opn");
-const uuid = require("uuid/v4");
-const os = require("os");
-const readline = require("readline-sync");
 const decompress = require("decompress");
-const choices = require("prompt-choices");
-const Enquirer = require("enquirer");
-const wrap = require("ansi-wrap");
 const program = require("commander");
-const simpleGit = require("simple-git");
-
-const proKeyFile = os.homedir() + "/.vaadin/proKey";
-class ProKey {
-  static async getProKey() {
-    if (fs.existsSync(proKeyFile)) {
-      return JSON.parse(fs.readFileSync(proKeyFile));
-    } else {
-      const proKey = await this.openBrowserAndWaitForKey();
-      if (proKey) {
-        fs.writeFileSync(proKeyFile, proKey);
-      }
-      return JSON.parse(proKey);
-    }
-  }
-
-  static async openBrowserAndWaitForKey() {
-    const uniqueId = uuid();
-
-    const url = "https://vaadin.com/pro/validate-license";
-    const loginUrl = url + "?connect=" + uniqueId;
-    const pollUrl = url + "/connect/" + uniqueId;
-
-    try {
-      console.log("The operation requires you to authenticate to vaadin.com.");
-      readline.question("Press enter to open your system browser to log in...");
-      opn(loginUrl, { wait: false });
-    } catch (e) {
-      console.error(
-        "Error opening system browser to validate license. Please open " +
-          loginUrl +
-          " manually",
-        e
-      );
-    }
-
-    try {
-      return this.listenForProKey(pollUrl);
-    } catch (e) {
-      throw e;
-    }
-  }
-
-  static async listenForProKey(url) {
-    const timeout = new Date().getTime() + 1000 * 60;
-
-    var data;
-    while (!data && new Date().getTime() < timeout) {
-      //            console.debug('Polling ' + url + ' for pro key');
-
-      await request(url, (error, response, body) => {
-        if (response && response.statusCode == 200) {
-          data = body;
-        }
-      }).catch((e) => {
-        // Server returns 404 if the login process has not completed
-      });
-    }
-    return data;
-  }
-}
 
 program
   .option(
     "--latest",
     "Use the latest release. By default uses the latest LTS release"
   )
+  .option(
+    "--fusion",
+    "Create a Fusion based project with TypeScript and LitElement"
+  )
   .option("--pre", "Use the latest pre release (if available)")
   .option(
     "--git",
     "Initialize a Git repository for the project and commit the initial files"
   )
-  .option(
-    "--tech [tech]",
-    "Use the specified tech stack [spring, javaee, osgi, plain-java]",
-    "spring"
-  )
   .arguments("<projectName>")
   .action(async function (projectName) {
-    const techStack = program.tech;
     const git = !!program.git;
-    const version = program.pre
-      ? "pre-release"
+    const preset = program.pre
+      ? "prerelease"
       : program.latest
       ? "latest"
+      : program.fusion
+      ? "fusion"
       : "lts";
-    const fs = require("fs");
     if (fs.existsSync(projectName)) {
       console.error("Directory '" + projectName + "' already exists");
       return;
     }
 
-    console.log(
-      `Creating application '${projectName}' using ${techStack} (${version})`
-    );
+    console.log(`Creating application '${projectName}' (${version})`);
 
     const options = {
-      qs: {
-        appName: projectName,
-        groupId: "com.example.app",
-        techStack: techStack,
-      },
       encoding: null,
       gzip: true,
-      /*      auth: {
-        user: proKey.username,
-        pass: proKey.proKey,
-        sendImmediately: false
-      }*/
     };
     await request.get(
-      `https://vaadin.com/vaadincom/start-service/${version}/project-base`,
+      `https://start.vaadin.com/dl?preset=${preset}`,
       options,
-      async function (error, response, body) {
+      async function (_error, response, body) {
         if (response && response.statusCode == 200) {
           fs.writeFileSync("temp.zip", body);
           await decompress("temp.zip", projectName, { strip: 1 });
