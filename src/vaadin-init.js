e8954b644546db9f6531d57be10d504d898a8943
Artur Signell <artur@vaadin.com>
2024-06-07 09:04:39 +0300

    feat: Ask user instead of use command line args

--- src/vaadin-init.js
+++ src/vaadin-init.js
@@ -1,111 +1,180 @@
-#!/usr/bin/env node
-
-"use strict";
 import { program } from "commander";
 import fs from "fs";
 import decompress from "decompress";
 import fetch from "node-fetch";
+import prompts from "prompts";
 
-program
-  .option("--flow", "Add Flow (Java) example views")
-  .option("--hilla", "Add Hilla (React + TypeScript) example views")
-  .option("--auth", "Add login/logout and access control")
-  .option("--db", "Add a database to the project")
-  .option("--kubernetes", "Include a Kubernetes configuration")
-  .option("--docker", "Include a Dockerfile")
-  .option("--pre", "Use the latest pre release (if available)")
-  .option(
-    "--git",
-    "Initialize a Git repository for the project and commit the initial files"
-  )
-  .option(
-    "--preset <preset>",
-    "Use the given preset you happen to know that exists"
-  )
-  .arguments("<projectName>")
-  .action(async function (projectName) {
-    const options = program.opts();
-    const git = !!options.git;
-    let preset = "base";
+function sanitizePath(path) {
+  return path?.trim().replace("/", "");
+}
+function isEmptyFolder(path) {
+  return fs.readdirSync(path).length === 0;
+}
+async function downloadProject(options) {
+  const { projectName } = options;
+  const presets = ["base"];
+  if (options.exampleViews === "flow") {
+    presets.push("partial-flow-example-views");
+  } else if (options.exampleViews === "hilla") {
+    presets.push("partial-hilla-example-views");
+  } else if (options.exampleViews === "hilla-flow") {
+    presets.push("partial-flow-example-views");
+    presets.push("partial-hilla-example-views");
+  }
 
-    if (options.preset) {
-      preset = options.preset;
+  for (const feature of options.features) {
+    if (feature === "git") {
+      continue;
+    } else if (feature === "pre") {
+      presets.push("partial-prerelease");
+      continue;
     }
 
-    if (options.flow) {
-      preset += "&preset=partial-flow-example-views";
-    }
-    if (options.hilla) {
-      preset += "&preset=partial-hilla-example-views";
-    }
+    presets.push(`partial-${feature}`);
+  }
 
-    if (options.auth) {
-      preset += "&preset=partial-auth";
-    }
+  const git = !!options.features.git;
 
-    if (options.db) {
-      preset += "&preset=partial-db";
-    }
+  if (fs.existsSync(projectName)) {
+    console.error("Directory '" + projectName + "' already exists");
+    return;
+  }
 
-    if (options.kubernetes) {
-      preset += "&preset=partial-kubernetes";
-    } else if (options.docker) {
-      preset += "&preset=partial-docker";
-    }
+  const preset = presets.map((p) => `preset=${p}`).join("&");
 
-    if (options.pre) {
-      preset += "&preset=partial-prerelease";
-    }
-
-    if (fs.existsSync(projectName)) {
-      console.error("Directory '" + projectName + "' already exists");
+  try {
+    const response = await fetch(
+      `https://start.vaadin.com/dl?${preset}&projectName=${projectName}`,
+      {
+        headers: {
+          "User-Agent": `Vaadin CLI`,
+          method: "GET",
+          "Accept-Encoding": "gzip",
+        },
+      }
+    );
+    if (!response.ok) {
+      if (response.status == 404) {
+        console.error("Preset not found");
+      } else {
+        console.error("Unable to create project: " + response.status);
+      }
       return;
     }
 
-    console.log(`Creating application '${projectName}'`);
-
+    const body = await response.arrayBuffer();
+    fs.writeFileSync("temp.zip", new Uint8Array(body));
     try {
-      const response = await fetch(
-        `https://start.vaadin.com/dl?preset=${preset}&projectName=${projectName}`,
-        {
-          headers: {
-            "User-Agent": `Vaadin CLI`,
-            method: "GET",
-            "Accept-Encoding": "gzip",
-          },
-        }
-      );
-      if (!response.ok) {
-        if (response.status == 404) {
-          console.error("Preset not found");
-        } else {
-          console.error("Unable to create project: " + response.status);
-        }
-        return;
-      }
-
-      const body = await response.arrayBuffer();
-      fs.writeFileSync("temp.zip", new Uint8Array(body));
-      try {
-        await decompress("temp.zip", ".", {
-          // workaround for https://github.com/kevva/decompress/issues/46
-          filter: (file) => !file.path.endsWith("/"),
-        });
-      } catch (e) {
-        console.error(e);
-        return;
-      }
-      fs.unlinkSync("temp.zip");
-      console.log("Project '" + projectName + "' created");
-      if (git) {
-        console.log("Creating Git repository and initial commit");
-        process.chdir(projectName);
-
-        require("simple-git")().init().add("./*").commit("Generated project");
-      }
+      await decompress("temp.zip", ".", {
+        // workaround for https://github.com/kevva/decompress/issues/46
+        filter: (file) => !file.path.endsWith("/"),
+      });
     } catch (e) {
-      console.error("Unable to create project: " + e);
+      console.error(e);
+      return;
     }
-  });
+    fs.unlinkSync("temp.zip");
+    console.log("");
+    console.log("Project '" + projectName + "' created");
+    console.log("");
+    console.log(`To run your project, open the ${projectName} folder in your IDE and launch the Application class`);
+    console.log("");
+    console.log("You can also run from the terminal using");
+    console.log("- cd " + projectName);
+    console.log("- mvn");
+    console.log("");
+    console.log(":");
 
-program.parse(process.argv);
+    if (git) {
+      console.log("Creating Git repository and initial commit");
+      process.chdir(projectName);
+
+      require("simple-git")().init().add("./*").commit("Generated project");
+    }
+  } catch (e) {
+    console.error("Unable to create project: " + e);
+  }
+}
+let result;
+let folder;
+
+try {
+  const argProjectName = sanitizePath(process.argv.slice(2)?.[0]);
+  result = await prompts(
+    [
+      {
+        type: argProjectName ? null : "text",
+        name: "projectName",
+        message: "Project name:",
+        initial: "my-vaadin-app",
+        onState: (state) => {
+          folder = sanitizePath(state.value);
+          console.log("Folder", folder);
+        },
+      },
+      {
+        type: () => null && 
+          fs.existsSync(folder) && !isEmptyFolder(folder) ? "select" : null,
+        name: "overwrite",
+        message: () => `Folder ${folder} already exists.`,
+        choices: [
+          { title: "Cancel", value: "no" },
+          { title: "Remove old project and create new", value: "yes" },
+        ],
+      },
+      {
+        type: (_, { overwrite }) => {
+          if (overwrite === "no") {
+            throw new Error("cancel");
+          }
+          return null;
+        },
+
+        onState: (state) => {
+          if (state.value === "no") {
+            throw new Error("Cancel");
+          }
+        },
+      },
+      {
+        type: "select",
+        name: "exampleViews",
+        message: "Example views:",
+        choices: [
+          { title: "Flow (Java)", value: "flow" },
+          { title: "Hilla (React + TypeScript)", value: "hilla" },
+          { title: "Flow and Hilla", value: "hilla-flow" },
+          { title: "None", value: "none" },
+        ],
+      },
+      {
+        type: "multiselect",
+        name: "features",
+        message: "Features:",
+        choices: [
+          { title: "Login/logout and access control", value: "auth" },
+          { title: "Include JPA support", value: "db" },
+          { title: "Kubernetes configuration", value: "kubernetes" },
+          { title: "Dockerfile", value: "docker" },
+          {
+            title:
+              "Initialize a Git repository for the project and commit the initial files",
+            value: "git",
+          },
+          { title: "Use pre release", value: "pre" },
+        ],
+      },
+    ],
+    {
+      onCancel: () => {
+        throw new Error("Cancel");
+      },
+    }
+  );
+
+  downloadProject(result);
+} catch (e) {
+  if (e?.message == "Cancel") {
+    console.log("Aborted");
+  }
+}
